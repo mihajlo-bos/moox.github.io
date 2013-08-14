919421b16b4adee80874b38a2cd61ae211484523
Maxime Thirouin <m@moox.io>
2013-08-14 09:47:39 +0200

    Updates

---
+++ assets/js/vanilla.js
@@ -0,0 +1,91 @@
+/*! MoOx - v8.0.0 - 2013-08-14 */
+/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
+window.matchMedia=window.matchMedia||(function(e,f){var c,a=e.documentElement,b=a.firstElementChild||a.firstChild,d=e.createElement("body"),g=e.createElement("div");g.id="mq-test-1";g.style.cssText="position:absolute;top:-100em";d.appendChild(g);return function(h){g.innerHTML='&shy;<style media="'+h+'"> #mq-test-1 { width: 42px; }</style>';a.insertBefore(d,b);c=g.offsetWidth==42;a.removeChild(d);return{matches:c,media:h}}})(document);
+/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with divs). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
+
+(function( w ){
+	
+	// Enable strict mode
+	"use strict";
+
+	w.picturefill = function() {
+		var ps = w.document.getElementsByTagName( "div" );
+		
+		// Loop the pictures
+		for( var i = 0, il = ps.length; i < il; i++ ){
+			if( ps[ i ].getAttribute( "data-picture" ) !== null ){
+
+				var sources = ps[ i ].getElementsByTagName( "div" ),
+					matches = [];
+			
+				// See if which sources match
+				for( var j = 0, jl = sources.length; j < jl; j++ ){
+					var media = sources[ j ].getAttribute( "data-media" );
+					// if there's no media specified, OR w.matchMedia is supported 
+					if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
+						matches.push( sources[ j ] );
+					}
+				}
+
+			// Find any existing img element in the picture element
+			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+
+			if( matches.length ){			
+				if( !picImg ){
+					picImg = w.document.createElement( "img" );
+					picImg.alt = ps[ i ].getAttribute( "data-alt" );
+					ps[ i ].appendChild( picImg );
+				}
+				
+				picImg.src =  matches.pop().getAttribute( "data-src" );
+			}
+			else if( picImg ){
+				ps[ i ].removeChild( picImg );
+			}
+		}
+		}
+	};
+	
+	// Run on resize and domready (w.load as a fallback)
+	if( w.addEventListener ){
+		w.addEventListener( "resize", w.picturefill, false );
+		w.addEventListener( "DOMContentLoaded", function(){
+			w.picturefill();
+			// Run once only
+			w.removeEventListener( "load", w.picturefill, false );
+		}, false );
+		w.addEventListener( "load", w.picturefill, false );
+	}
+	else if( w.attachEvent ){
+		w.attachEvent( "onload", w.picturefill );
+	}
+	
+}( this ));
+// just it case scripts below breack everything on a browser, just put the css like if there is no js
+document.documentElement.classList.toggle('js');
+document.documentElement.classList.toggle('no-js');
+
+// my website
+
+// call me just before bottom, I'll be dom ready :p
+[].forEach.call(document.querySelectorAll('[href="#not-ready"]'), function(el){
+	el.setAttribute('data-hint', 'Not ready yet !');
+});
+
+[].forEach.call(document.querySelectorAll('.js-togglable'), function(el){
+	var toggler = el.querySelector('.js-togglable__toggler');
+	var items = [].slice.call(el.querySelectorAll('.js-togglable__item'));
+	
+	var toggle = function(event) {
+		items.forEach(function(item) {
+			item.classList.toggle('js-togglable__item--hide');
+		});
+	};
+	
+	toggler.addEventListener('click', toggle);
+	toggler.addEventListener('keyup', toggle);
+});
+
+// everything run fine, let's tell this to our stylesheets
+document.documentElement.classList.toggle('js');
+document.documentElement.classList.toggle('no-js');
